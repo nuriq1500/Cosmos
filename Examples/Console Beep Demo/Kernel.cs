ff28d7c2ef0e55e537fc34e670182c2244ce4e42
nuriq1500 <merhababenlyrex@outlook.com>
2024-07-30 17:52:43 +0300

    Update Kernel.cs

--- Examples/Console Beep Demo/Kernel.cs
+++ Examples/Console Beep Demo/Kernel.cs
@@ -10,7 +10,7 @@ namespace BeepDemo
     {
 		protected override void BeforeRun()
 		{
-			Console.WriteLine("Cosmos booted successfully. Type a line of text to get it echoed back.");	
+			Console.WriteLine("Tea booted successfully. Type a line of text to get it echoed back.");	
 		}
         protected override void Run()
         {
